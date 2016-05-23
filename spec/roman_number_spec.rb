8b891f04d2a389ee2bd83c64fc9bdf3d82a2a3a7
Iran <iranthau@gmail.com>
2016-05-24 09:54:12 +1000

    Fix the number 9 bug

--- spec/roman_number_spec.rb
+++ spec/roman_number_spec.rb
@@ -7,11 +7,15 @@ describe RomanNumber do
       2     => 'II',
       4     => 'IV',
       5     => 'V',
+      9     => 'IX',
       10    => 'X',
       40    => 'XL',
       50    => 'L',
+      90    => 'XC',
       100   => 'C',
       500   => 'D',
+      900   => 'CM',
+      999   => 'CMXCIX',
       1000  => 'M',
       4376  => 'MMMMCCCLXXVI'
     }.each do |number, roman_number|
