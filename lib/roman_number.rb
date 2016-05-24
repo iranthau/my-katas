1028f67a87cf49908ade41f76ce55b4eeff95b87
Iran <iranthau@gmail.com>
2016-05-24 14:35:31 +1000

    Generate roman numbers using a loop instead of recursive function

--- lib/roman_number.rb
+++ lib/roman_number.rb
@@ -4,6 +4,7 @@ class RomanNumber
     1000  => 'M',
     900   => 'CM',
     500   => 'D',
+    400   => 'CD',
     100   => 'C',
     90    => 'XC',
     50    => 'L',
@@ -15,14 +16,33 @@ class RomanNumber
     1     => 'I'
   }.freeze
 
-  def self.roman(number)
-    return '' if number.zero?
-    return 'number is out of range' if number >= 5000
-    arabic, roman_number = conversion_factors_for(number)
-    roman_number + roman(number - arabic)
+  def self.roman_loop(number = 0)
+    return 'invalid number' unless valid?(number)
+    roman(number)
   end
 
-  def self.conversion_factors_for(number)
-    ROMAN_MAP.find { |arabic, _| arabic <= number }
+  def self.roman(number)
+    result = ''
+    ROMAN_MAP.each do |arabic, roman_number|
+      result << roman_number * (number / arabic)
+      number = number % arabic
+    end
+    result
+  end
+
+  # def self.roman(number = 0)
+  #   return 'invalid data type' unless number.is_a?(Numeric)
+  #   return '' if number.zero?
+  #   return 'invalid number' unless valid?(number)
+  #   arabic, roman_number = conversion_factors_for(number)
+  #   roman_number + roman(number - arabic)
+  # end
+
+  # def self.conversion_factors_for(number)
+  #   ROMAN_MAP.find { |arabic, _| arabic <= number }
+  # end
+
+  def self.valid?(number)
+    number > 0 && number < 5000
   end
 end
--- spec/roman_number_spec.rb
+++ spec/roman_number_spec.rb
@@ -13,26 +13,40 @@ describe RomanNumber do
       50    => 'L',
       90    => 'XC',
       100   => 'C',
+      445   => 'CDXLV',
       500   => 'D',
       900   => 'CM',
       999   => 'CMXCIX',
       1000  => 'M',
+      21    => 'XXI',
       4376  => 'MMMMCCCLXXVI'
     }.each do |number, roman_number|
       it "converts the number #{number} to the string #{roman_number}" do
-        expect(described_class.roman(number)).to eq(roman_number)
+        expect(described_class.roman_loop(number)).to eq(roman_number)
+      end
+    end
+
+    context 'when call with nothing' do
+      it 'returns invalid number' do
+        expect(described_class.roman_loop).to eq('invalid number')
+      end
+    end
+
+    context 'when the given number is a negative number' do
+      it 'returns invalid number' do
+        expect(described_class.roman_loop(-1)).to eq('invalid number')
       end
     end
 
     context 'when the given number is 0' do
       it 'returns nothing' do
-        expect(described_class.roman(0)).to eq('')
+        expect(described_class.roman_loop(0)).to eq('invalid number')
       end
     end
 
     context 'when the given number is greater than 4999' do
-      it 'returns out of range message' do
-        expect(described_class.roman(5000)).to eq('number is out of range')
+      it 'returns invalid number' do
+        expect(described_class.roman_loop(5000)).to eq('invalid number')
       end
     end
   end
