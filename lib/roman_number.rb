337e9ad17ed679f946253344b699c734191c63d0
Iran <iranthau@gmail.com>
2016-05-31 14:35:15 +1000

    Final commit

--- lib/roman_number.rb
+++ lib/roman_number.rb
@@ -22,25 +22,12 @@ class RomanNumber
   end
 
   def self.roman(number)
-    result = ''
-    ROMAN_MAP.each do |arabic, roman_number|
-      result << roman_number * (number / arabic)
+    ROMAN_MAP.map do |arabic, roman_number|
+      result = roman_number * (number / arabic)
       number = number % arabic
-    end
-    result
+      result
+    end.join
   end
-
-  # def self.roman(number = 0)
-  #   return 'invalid data type' unless number.is_a?(Numeric)
-  #   return '' if number.zero?
-  #   return 'invalid number' unless valid?(number)
-  #   arabic, roman_number = conversion_factors_for(number)
-  #   roman_number + roman(number - arabic)
-  # end
-
-  # def self.conversion_factors_for(number)
-  #   ROMAN_MAP.find { |arabic, _| arabic <= number }
-  # end
 
   def self.valid?(number)
     number > 0 && number < 5000
